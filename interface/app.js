06d21bbf7b497fb24df3a71aa62f4c07f3c506dd
Kumar Ravi <kumarravi@Kumars-MacBook-Air.local>
2018-01-15 16:58:05 +0530

    added interface

---
+++ interface/app.js
@@ -0,0 +1,38 @@
+var http = require('http');
+var formidable = require('formidable');
+var fs = require('fs');
+const image2base64 = require('image-to-base64');
+
+http.createServer(function (req, res) {
+  if (req.url == '/fileupload') {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      var oldpath = files.filetoupload.path;
+      var newpath = '/Users/kumarravi/project/derm-ai/interface/uploaded/' + files.filetoupload.name;
+      fs.rename(oldpath, newpath, function (err) {
+        if (err) throw err;
+        image2base64(newpath)
+    .then(
+        (response) => {
+            console.log(response); //cGF0aC90by9maWxlLmpwZw==
+        }
+    )
+    .catch(
+        (error) => {
+            console.log(error); //Exepection error....
+        }
+    )
+
+        res.write('File uploaded and moved!');
+        res.end();
+      });
+ });
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
+    return res.end();
+  }
+}).listen(8080);
